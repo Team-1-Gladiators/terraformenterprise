109986aad17800ae44df57a9a8de12dd3df4445c
njingu90 <85954427+njingu90@users.noreply.github.com>
2021-12-08 14:57:41 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -92,3 +92,143 @@ resource "aws_security_group" "project" {
     Name = "Ec2-Security-group"
   }
 }
+resource "aws_instance" "project0" {
+  ami           = "ami-00dfe2c7ce89a450b" 
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.project-0.id
+  security_groups = [aws_security_group.project.id]
+  depends_on = [aws_internet_gateway.project]
+  availability_zone = "us-east-2a"
+  associate_public_ip_address = true
+  user_data = <<-EOF
+              #!/bin/bash 
+             yum update –y 
+             amazon-linux-extras install nginx1.12
+             nginx -v
+             systemctl start nginx
+             systemctl enable nginx
+             chmod 2775 /usr/share/nginx/html 
+             find /usr/share/nginx/html -type d -exec chmod 2775 {} \;
+             find /usr/share/nginx/html -type f -exec chmod 0664 {} \;
+             echo "<h3> "Hello from Kecy Nji 1a"</h3>" > /usr/share/nginx/html/index.html
+     EOF
+
+tags = {
+    Name = "webserver"
+  }
+}
+resource "aws_instance" "project1" {
+  ami           = "ami-00dfe2c7ce89a450b" 
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.project-1.id
+  security_groups = [aws_security_group.project.id]
+  depends_on = [aws_internet_gateway.project]
+  availability_zone = "us-east-2b"
+  associate_public_ip_address = true
+  user_data = <<-EOF
+              #!/bin/bash 
+             yum update –y 
+             amazon-linux-extras install nginx1.12
+             nginx -v
+             systemctl start nginx
+             systemctl enable nginx
+             chmod 2775 /usr/share/nginx/html 
+             find /usr/share/nginx/html -type d -exec chmod 2775 {} \;
+             find /usr/share/nginx/html -type f -exec chmod 0664 {} \;
+             echo "<h3> "Hello from Kecy Nji 1b"</h3>" > /usr/share/nginx/html/index.html
+     EOF
+
+tags = {
+    Name = "webserver"
+  }
+}
+resource "aws_security_group" "project-lb" {
+  name        = "alb-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.project.id
+
+  ingress {
+      description      = "http from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      
+    }
+
+  egress{
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      
+    }
+  
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+resource "aws_lb" "project" {
+  name               = "project-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.project-lb.id,aws_security_group.project.id]
+  
+  enable_deletion_protection = false
+
+  subnet_mapping {
+    subnet_id     = aws_subnet.project-0.id
+   }
+  subnet_mapping {
+    subnet_id     = aws_subnet.project-1.id
+    
+  }
+
+  tags = {
+    name = "project-load-balancer"
+  }
+}
+
+resource "aws_lb_target_group" "project" {
+  name     = "webserver"
+  target_type = "instance"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.project.id
+  health_check {
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+    timeout = 5
+    interval = 20
+    matcher = "200,302"
+    path = "/"
+    port = "traffic-port"
+     
+  }
+
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.project.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.project.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "project0" {
+  target_group_arn = aws_lb_target_group.project.arn
+  target_id        = aws_instance.project0.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "project1" {
+  target_group_arn = aws_lb_target_group.project.arn
+  target_id        = aws_instance.project1.id
+  port             = 80
+}
